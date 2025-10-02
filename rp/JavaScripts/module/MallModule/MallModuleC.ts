8183e080cf9880646fab819617cef79caa628d56
侯凯召 <3046916186@qq.com>
2025-10-02 19:06:35 +0800

    Wish弹窗提示

--- rp/JavaScripts/module/MallModule/MallModuleC.ts
+++ rp/JavaScripts/module/MallModule/MallModuleC.ts
@@ -10,6 +10,7 @@ import Utils from "../../tools/Utils";
 import ExecutorManager from "../../tools/WaitingQueue";
 import { CharacterModuleC } from "../CharacterModule/CharacterModuleC";
 import { HUDModuleC } from "../HUDModule/HUDModule";
+import GetWishPanel from "../WishModule/ui/GetWishPanel";
 import { WishDataV0 } from "../WishModule/WishData";
 import WishModuleC from "../WishModule/WishModuleC";
 import Mall from "./Mall";
@@ -2059,11 +2060,12 @@ export default class MallModuleC extends ModuleC<MallModuleS, MallData> {
         return this.mallConfigData.addVipCoinNumber;
     }
 
-    public async updateNickWish(wishDataV0: WishDataV0): Promise<void> {
-        await this.server.net_updateNickWish(wishDataV0);
+    public async updateNickWish(wishDataV0: WishDataV0, isGive: boolean): Promise<void> {
+        await this.server.net_updateNickWish(wishDataV0, isGive);
     }
 
-    public net_giveSuccess(): void {
-        Notice.showDownNotice(`好友帮你购买成功`);
+    public net_giveSuccess(wishDataV0: WishDataV0): void {
+        Notice.showDownNotice(`你的心愿单好友帮你购买成功`);
+        UIService.getUI(GetWishPanel).showGetWishPanel(wishDataV0);
     }
 }
